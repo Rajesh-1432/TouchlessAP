9f0f972dd275674b8181f63312bed45ca178d8bc
Rajesh-1432 <rajeshmca1432@gmail.com>
2024-02-08 18:27:34 +0530

    insert into git without nodemodules

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -4,17 +4,16 @@ const url="mongodb://localhost:27017/";
 const dbName="tlsAp";
 
 let client;
-const connect=async()=>{
-    try{
-        client=await MongoClient.connect(url,{useUnifiedTopology:true});
-        console.log("DataBase Connected Sucessfully");
+const connect = async () => {
+    try {
+        const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+        console.log("Database Connected Successfully");
         return client.db(dbName);
-    }
-    catch(error){
-        console.error("Faild To connect DataBase:",error);    
+    } catch (error) {
+        console.error("Failed To Connect Database:", error);
         throw error;
     }
-};
+};  
 
 const close=()=>{
     if(client){
