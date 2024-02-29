b02e9d3f389bf09e543fd7469c327dbc6fd73431
Rajesh-1432 <125548003+Rajesh-1432@users.noreply.github.com>
2024-02-29 20:28:17 +0530

    Update LoginPage.jsx

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -10,7 +10,7 @@ const LoginPage = () => {
   const navigate = useNavigate();
 
   const handleSubmit = (values) => {
-    // Custom validation for a specific username and passwords
+    // Custom validation for a specific username and password
     const expectedUsername = "tail@gmail.com";
     const expectedPassword = "1234567890";
 
